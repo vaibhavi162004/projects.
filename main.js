79e89f26757b9e1171dd04ee1937a716b7012061
vaibhavi162004 <vaibhavish4@gmail.com>
2024-07-02 22:39:50 +0530

    Responsive landing web page

--- main.js
+++ main.js
@@ -1,65 +1,14 @@
-let timer;
-let isRunning = false;
-let hours = 0;
-let minutes = 0;
-let seconds = 0;
+document.addEventListener("DOMContentLoaded", function(){
+const navbar=document.querySelector('.navbar');
 
-const display = document.getElementById('display');
-const startBtn = document.getElementById('startStopBtn');
-const pauseBtn = document.getElementById('pauseBtn');
-const resetBtn = document.getElementById('resetBtn');
+window.addEventListener('scroll', function(){
+    if(window.scroll>50){
+        navbar.style.backgroundcolor ='#555';
 
-function updateDisplay() {
-    display.textContent = 
-        `${hours.toString().padStart(2, '0')}:${minutes.toString().padStart(2, '0')}:${seconds.toString().padStart(2, '0')}`;
-}
-
-function start() {
-    if (!isRunning) {
-        timer = setInterval(() => {
-            seconds++;
-            if (seconds === 60) {
-                seconds = 0;
-                minutes++;
-                if (minutes === 60) {
-                    minutes = 0;
-                    hours++;
-                }
-            }
-            updateDisplay();
-        }, 1000);
-        isRunning = true;
-        startBtn.textContent = 'Pause';
+    } else{
+        navbar.style.backgroundcolor ='#777';
+        
     }
-}
 
-function pause() {
-    if (isRunning) {
-        clearInterval(timer);
-        isRunning = false;
-        startBtn.textContent = 'Start';
-    }
-}
-
-function reset() {
-    clearInterval(timer);
-    hours = 0;
-    minutes = 0;
-    seconds = 0;
-    isRunning = false;
-    updateDisplay();
-    startBtn.textContent = 'Start';
-}
-
-startBtn.addEventListener('click', () => {
-    if (isRunning) {
-        pause();
-    } else {
-        start();
-    }
 });
-
-resetBtn.addEventListener('click', reset);
-
-updateDisplay();
-
+});
